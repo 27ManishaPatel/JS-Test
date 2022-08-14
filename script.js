1b83aea9a74d84a7d3a05fe917b787f6a7ba6305
HackYourFuture DK <manisha270989@gmail.com>
2022-08-14 11:32:34 +0200

    changes in 1st exercise

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function logOutNumbers(){
     for (let i = 0; i < 101 ; i++){
         if(i % 3 == 0 && i % 5 == 0){
             console.log("Jackpot")
-        }
-        if(i % 3 == 0){
+        }else if(i % 3 == 0){
             console.log("This is divisible by 3")
         }else if(i % 5 == 0){
             console.log("This is divisible by 5")
